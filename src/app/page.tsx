5e49ca21689034726b8f395ba396dbc18f7aacb1
clarencepenz <clarencedespenz@gmail.com>
2025-10-06 11:17:13 +0100

    chore: ga run

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
 
   return (
     <div className="p-8 max-w-xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Todos</h1>
+      <h1 className="text-2xl font-bold mb-4">Todos x</h1>
 
       <div className="flex gap-2 mb-4">
         <input
